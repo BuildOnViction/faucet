8146633df213c310e14d04c9dc72bf506b24a570
Bob Jr <nguyennt@coin98.finance>
2023-12-20 15:20:02 +0700

    remove config file import

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -1,36 +1,35 @@
-const config = require('../../../../config.json')
-$(function() {
+$(function () {
 	var loader = $(".loading-container");
-	$( "#faucetForm" ).submit(function( e ) {
+	$("#faucetForm").submit(function (e) {
 		e.preventDefault();
-    	$this = $(this);
+		$this = $(this);
 		loader.removeClass("hidden");
 		var receiver = $("#receiver").val();
-    var captcha =  $("#g-recaptcha-response").val();
+		var captcha = $("#g-recaptcha-response").val();
 		$.ajax({
-		  	url:"/",
-		  	type:"POST",
-		  	data: {
-		  		receiver: receiver,
-          captcha: captcha
-		  	}
-		}).done(function(data) {
+			url: "/",
+			type: "POST",
+			data: {
+				receiver: receiver,
+				captcha: captcha
+			}
+		}).done(function (data) {
 			if (!data.success) {
 				loader.addClass("hidden");
 				swal(data.error.title, data.error.message, "error");
 				return;
 			}
 
-			getTxCallBack(data.success.txHash, function() {
+			getTxCallBack(data.success.txHash, function () {
 				$("#receiver").val('');
 				loader.addClass("hidden");
 				swal("Success",
-				  "15 VIC is successfully transfered to " + receiver +` in Tx<br /><a href='${config.Ethereum.live.explorer}/tx/` + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
-				  "success"
+					"15 VIC is successfully transfered to " + receiver + ` in Tx<br /><a href='https://testnet.vicscan.xyz/tx/` + data.success.txHash + "' target='_blank'>" + data.success.txHash + "</a>",
+					"success"
 				);
 				grecaptcha.reset();
 			});
-		}).fail(function(err) {
+		}).fail(function (err) {
 			loader.addClass("hidden");
 		});
 	});
